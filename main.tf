f8fbdc4040b08195eafc6b851b8ae6fb518a6240
Josh Sizer <joshsizer@gmail.com>
2023-08-19 11:36:59 -0400

    config: add non-default vpc & subnet

--- main.tf
+++ main.tf
@@ -16,8 +16,28 @@ provider "aws" {
 resource "aws_instance" "slurm_control" {
   ami           = "ami-08a52ddb321b32a8c"
   instance_type = "t3.micro"
+  subnet_id     = aws_subnet.slurm_subnet_us_east_1a.id
 
   tags = {
-    Name = "slurm-control"
+    Name = "slurm-controller"
+  }
+}
+
+resource "aws_vpc" "slurm_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "slurm_vpc"
+  }
+}
+
+resource "aws_subnet" "slurm_subnet_us_east_1a" {
+  vpc_id            = aws_vpc.slurm_vpc.id
+  cidr_block        = "10.0.0.0/16"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "slurm_subnet_us_east_1a"
   }
 }
