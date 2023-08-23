da9f8a9674cd66c02fb6e39bde543e8d6deaf7d7
Josh Sizer <joshsizer@gmail.com>
2023-08-23 17:44:25 -0400

    config: add ssh security group

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "aws_instance" "slurm_control" {
   ami           = "ami-08a52ddb321b32a8c"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.slurm_subnet_us_east_1a.id
+  security_groups = ["${aws_security_group.slurm_ssh.id}"]
 
   tags = {
     Name = "slurm-controller"
@@ -39,5 +40,32 @@ resource "aws_subnet" "slurm_subnet_us_east_1a" {
 
   tags = {
     Name = "slurm_subnet_us_east_1a"
+  }
+}
+
+resource "aws_security_group" "slurm_ssh" {
+  name        = "slurm_ssh"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.slurm_vpc.id
+
+  ingress {
+    description      = "SSH from anywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "slurm_ssh"
   }
 }
